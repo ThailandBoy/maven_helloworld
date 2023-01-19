f93e0d61a85ed00be7ad5728f0343bd79bc8b9e3
ThailandBoy <91521829+ThailandBoy@users.noreply.github.com>
2023-01-19 08:27:07 +0300

    second commit, some troubles

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        System.out.println( "i got some trouble \"Module SDK 19 does not support source version 1.5.\"!" );
     }
 }
